fabbd52f8bc51b5753eec90cc888fce72c284cf5
ADMIN <ADMIN@DESKTOP-4A2FR2S>
2026-01-18 21:27:46 +0530

    Add project files.

---
+++ project3/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace project3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            Rollnotxt = new TextBox();
+            dataGridView1 = new DataGridView();
+            Nametxt = new TextBox();
+            label3 = new Label();
+            Divtxt = new TextBox();
+            label4 = new Label();
+            Addresstxt = new TextBox();
+            label5 = new Label();
+            Contacttxt = new TextBox();
+            label6 = new Label();
+            insertbtn = new Button();
+            Clrbtn = new Button();
+            cross = new Label();
+            delbtnn = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(59, 47, 47);
+            label1.Location = new Point(364, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(182, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Student's Database";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Sitka Banner", 14.2499981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(47, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Roll No";
+            // 
+            // Rollnotxt
+            // 
+            Rollnotxt.Location = new Point(47, 118);
+            Rollnotxt.Name = "Rollnotxt";
+            Rollnotxt.Size = new Size(134, 23);
+            Rollnotxt.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(248, 239);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(433, 251);
+            dataGridView1.TabIndex = 3;
+            // 
+            // Nametxt
+            // 
+            Nametxt.Location = new Point(218, 118);
+            Nametxt.Name = "Nametxt";
+            Nametxt.Size = new Size(137, 23);
+            Nametxt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Sitka Banner", 14.2499981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(218, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Student Name";
+            // 
+            // Divtxt
+            // 
+            Divtxt.Location = new Point(395, 118);
+            Divtxt.Name = "Divtxt";
+            Divtxt.Size = new Size(137, 23);
+            Divtxt.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Sitka Banner", 14.2499981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(395, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 28);
+            label4.TabIndex = 6;
+            label4.Text = "Division";
+            // 
+            // Addresstxt
+            // 
+            Addresstxt.Location = new Point(562, 118);
+            Addresstxt.Name = "Addresstxt";
+            Addresstxt.Size = new Size(135, 23);
+            Addresstxt.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Sitka Banner", 14.2499981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(562, 87);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 28);
+            label5.TabIndex = 8;
+            label5.Text = "Address";
+            // 
+            // Contacttxt
+            // 
+            Contacttxt.Location = new Point(730, 118);
+            Contacttxt.Name = "Contacttxt";
+            Contacttxt.Size = new Size(135, 23);
+            Contacttxt.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Sitka Banner", 14.2499981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(730, 87);
+            label6.Name = "label6";
+            label6.Size = new Size(93, 28);
+            label6.TabIndex = 10;
+            label6.Text = "Contact No";
+            // 
+            // insertbtn
+            // 
+            insertbtn.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            insertbtn.Location = new Point(248, 179);
+            insertbtn.Name = "insertbtn";
+            insertbtn.Size = new Size(93, 41);
+            insertbtn.TabIndex = 12;
+            insertbtn.Text = "Insert";
+            insertbtn.UseVisualStyleBackColor = true;
+            insertbtn.Click += insertbtn_Click;
+            // 
+            // Clrbtn
+            // 
+            Clrbtn.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Clrbtn.Location = new Point(588, 179);
+            Clrbtn.Name = "Clrbtn";
+            Clrbtn.Size = new Size(93, 41);
+            Clrbtn.TabIndex = 15;
+            Clrbtn.Text = "Clear";
+            Clrbtn.UseVisualStyleBackColor = true;
+            Clrbtn.Click += Clrbtn_Click;
+            // 
+            // cross
+            // 
+            cross.AutoSize = true;
+            cross.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cross.ForeColor = SystemColors.ButtonHighlight;
+            cross.Location = new Point(887, -1);
+            cross.Name = "cross";
+            cross.Size = new Size(30, 32);
+            cross.TabIndex = 16;
+            cross.Text = "X";
+            cross.Click += cross_Click;
+            // 
+            // delbtnn
+            // 
+            delbtnn.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            delbtnn.Location = new Point(414, 179);
+            delbtnn.Name = "delbtnn";
+            delbtnn.Size = new Size(93, 41);
+            delbtnn.TabIndex = 17;
+            delbtnn.Text = "Delete";
+            delbtnn.UseVisualStyleBackColor = true;
+            delbtnn.Click += delbtnn_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = Color.LightPink;
+            ClientSize = new Size(917, 539);
+            Controls.Add(delbtnn);
+            Controls.Add(cross);
+            Controls.Add(Clrbtn);
+            Controls.Add(insertbtn);
+            Controls.Add(Contacttxt);
+            Controls.Add(label6);
+            Controls.Add(Addresstxt);
+            Controls.Add(label5);
+            Controls.Add(Divtxt);
+            Controls.Add(label4);
+            Controls.Add(Nametxt);
+            Controls.Add(label3);
+            Controls.Add(dataGridView1);
+            Controls.Add(Rollnotxt);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox Rollnotxt;
+        private DataGridView dataGridView1;
+        private TextBox Nametxt;
+        private Label label3;
+        private TextBox Divtxt;
+        private Label label4;
+        private TextBox Addresstxt;
+        private Label label5;
+        private TextBox Contacttxt;
+        private Label label6;
+        private Button insertbtn;
+        private Button Clrbtn;
+        private Label cross;
+        private Button delbtnn;
+    }
+}
---
+++ project3/Form1.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace project3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            con = new SqlConnection(
+                @"Data Source=DESKTOP-4A2FR2S\SQLEXPRESS;
+                  Initial Catalog=student;
+                  Integrated Security=True;
+                  Trust Server Certificate=True"
+            );
+
+            // IMPORTANT SETTINGS
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+        }
+
+        // 🔹 FORM LOAD
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayStudents();
+        }
+
+        // 🔹 DISPLAY DATA
+        private void DisplayStudents()
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM STU", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // 🔹 CLEAR TEXTBOXES
+        private void ResetFields()
+        {
+            Rollnotxt.Clear();
+            Nametxt.Clear();
+            Divtxt.Clear();
+            Addresstxt.Clear();
+            Contacttxt.Clear();
+        }
+
+        // 🔹 INSERT
+        private void insertbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    @"INSERT INTO STU (RollNo, Sname, Division, SAddress, Contact)
+                      VALUES (@RollNo, @Sname, @Division, @SAddress, @Contact)", con);
+
+                cmd.Parameters.AddWithValue("@RollNo", Rollnotxt.Text);
+                cmd.Parameters.AddWithValue("@Sname", Nametxt.Text);
+                cmd.Parameters.AddWithValue("@Division", Divtxt.Text);
+                cmd.Parameters.AddWithValue("@SAddress", Addresstxt.Text);
+                cmd.Parameters.AddWithValue("@Contact", Contacttxt.Text);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Student inserted successfully");
+                DisplayStudents();
+                ResetFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // 🔹 UPDATE
+        private void Updatebtn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please double-click a student to update");
+                return;
+            }
+
+            int studentId = Convert.ToInt32(
+                dataGridView1.SelectedRows[0].Cells["StudentId"].Value);
+
+            try
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    @"UPDATE STU SET 
+                        RollNo=@RollNo,
+                        Sname=@Sname,
+                        Division=@Division,
+                        SAddress=@SAddress,
+                        Contact=@Contact
+                      WHERE StudentId=@StudentId", con);
+
+                cmd.Parameters.AddWithValue("@StudentId", studentId);
+                cmd.Parameters.AddWithValue("@RollNo", Rollnotxt.Text);
+                cmd.Parameters.AddWithValue("@Sname", Nametxt.Text);
+                cmd.Parameters.AddWithValue("@Division", Divtxt.Text);
+                cmd.Parameters.AddWithValue("@SAddress", Addresstxt.Text);
+                cmd.Parameters.AddWithValue("@Contact", Contacttxt.Text);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Student updated successfully");
+                DisplayStudents();
+                ResetFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // 🔹 DELETE
+        private void Delbtnn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a student to delete");
+                return;
+            }
+
+            int studentId = Convert.ToInt32(
+                dataGridView1.SelectedRows[0].Cells["StudentId"].Value);
+
+            try
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM STU WHERE StudentId=@StudentId", con);
+
+                cmd.Parameters.AddWithValue("@StudentId", studentId);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Student deleted successfully");
+                DisplayStudents();
+                ResetFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // 🔹 DOUBLE CLICK → LOAD DATA INTO TEXTBOXES (KEY PART)
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                Rollnotxt.Text = row.Cells["RollNo"].Value.ToString();
+                Nametxt.Text = row.Cells["Sname"].Value.ToString();
+                Divtxt.Text = row.Cells["Division"].Value.ToString();
+                Addresstxt.Text = row.Cells["SAddress"].Value.ToString();
+                Contacttxt.Text = row.Cells["Contact"].Value.ToString();
+            }
+        }
+
+        // 🔹 CLEAR BUTTON
+        private void Clrbtn_Click(object sender, EventArgs e)
+        {
+            ResetFields();
+        }
+
+        private void cross_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void delbtnn_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a student to delete");
+                return;
+            }
+
+            int studentId = Convert.ToInt32(
+                dataGridView1.SelectedRows[0].Cells["StudentId"].Value);
+
+            try
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM STU WHERE StudentId=@StudentId", con);
+
+                cmd.Parameters.AddWithValue("@StudentId", studentId);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Student deleted successfully");
+                DisplayStudents();
+                ResetFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
